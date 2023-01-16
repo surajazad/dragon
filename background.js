1fb980a4f799d3543484eddc2e9cd8bd7e015103
Suraj Azad <surajkantazad@gmail.com>
2023-01-17 00:44:18 +0530

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-console.log("Hi from background Script file")
-
-chrome.runtime.onMessage.addListener( function (request, sender, sendResponse) {
-    console.log("Got message from content Script: ", request);
-    sendResponse('OK');
-})
